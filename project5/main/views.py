a9fe74ea2d9aeb3a219baa9dc89fc29c5acea9cb
Dmitry N Arkhipov <arkhipov@alfasatcom.ru>
2021-01-16 00:20:57 +0300

    - в работе

--- project5/main/views.py
+++ project5/main/views.py
@@ -5,6 +5,11 @@ from flask import Blueprint, render_template, redirect, url_for, session
 from .models import Category, Meal
 
 
+def read_session_cart() -> list:
+    cart = session.get('cart')
+    return list() if cart is None or len(cart) == 0 else cart.split(',')
+
+
 blueprint = Blueprint('main', __name__, template_folder='templates')
 
 
@@ -18,20 +23,44 @@ def get_main_page():
     if promo is not None:
         menu = {'новинки': promo, **menu}
 
+    cart_meals = Meal.query.filter(Meal.id.in_(read_session_cart())).all()
+
     return render_template(
         'index.html',
-        menu=menu
+        menu=menu,
+        cart_meals=cart_meals
     )
 
 
-@blueprint.route('/addtocart/<int:meal_id>')
-def get_addtocart(meal_id: int):
-    session['cart'] = meal_id
+@blueprint.route('/addtocart/<meal_id>')
+def add_to_cart(meal_id):
+    meal = Meal.query.get_or_404(meal_id)
+
+    cart = set(read_session_cart())
+    cart.add(meal_id)
+    session['cart'] = ','.join(list(cart))
+
+    return redirect(url_for('main.get_cart'))
+
+
+@blueprint.route('/removefromcart/<meal_id>')
+def remove_from_cart(meal_id):
+    cart = read_session_cart()
+    if len(cart) > 0:
+        try:
+            cart.remove(meal_id)
+        except ValueError:
+            pass
+    session['cart'] = ','.join(cart)
+
     return redirect(url_for('main.get_cart'))
 
 
 @blueprint.route('/cart')
 def get_cart():
+    cart_meals = Meal.query.filter(Meal.id.in_(read_session_cart())).all()
+
     return render_template(
-        'cart.html'
+        'cart.html',
+        cart_meals=cart_meals
     )
