e2bf1b6589fba0c8330974278eb142d44531acdc
Dmitry N Arkhipov <arkhipov@alfasatcom.ru>
2021-01-15 17:46:45 +0300

    - в работе

--- project5/main/views.py
+++ project5/main/views.py
@@ -1,6 +1,6 @@
 """Main site views."""
 import random
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for, session
 
 from .models import Category, Meal
 
@@ -8,7 +8,7 @@ from .models import Category, Meal
 blueprint = Blueprint('main', __name__, template_folder='templates')
 
 
-@blueprint.route('/', methods=('get',))
+@blueprint.route('/')
 def get_main_page():
     categories = Category.query.order_by(Category.title).all()
     menu = {category.title.lower(): random.sample(category.meals, 3) for category in categories}
@@ -21,4 +21,17 @@ def get_main_page():
     return render_template(
         'index.html',
         menu=menu
+    )
+
+
+@blueprint.route('/addtocart/<int:meal_id>')
+def get_addtocart(meal_id: int):
+    session['cart'] = meal_id
+    return redirect(url_for('main.get_cart'))
+
+
+@blueprint.route('/cart')
+def get_cart():
+    return render_template(
+        'cart.html'
     )
